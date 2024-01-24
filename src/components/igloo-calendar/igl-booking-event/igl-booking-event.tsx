301eec62e8d8ab0ac17b6ba039a224fe837590c0
David Chowaifaty <chowaifaty.d@gmail.com>
2024-01-24 18:03:39 +0200

    fixed the border style for the unassigned units

--- src/components/igloo-calendar/igl-booking-event/igl-booking-event.tsx
+++ src/components/igloo-calendar/igl-booking-event/igl-booking-event.tsx
@@ -618,7 +618,11 @@ export class IglBookingEvent {
         {/* onMouseOver={() =>this.showEventInfo(true)}  */}
         <div
           class={`bookingEventBase ${
-            !this.bookingEvent.is_direct && !isBlockUnit(this.bookingEvent.STATUS_CODE) && this.bookingEvent.STATUS !== 'TEMP-EVENT' && 'border border-dark'
+            !this.bookingEvent.is_direct &&
+            !isBlockUnit(this.bookingEvent.STATUS_CODE) &&
+            this.bookingEvent.STATUS !== 'TEMP-EVENT' &&
+            this.bookingEvent.ID !== 'NEW_TEMP_EVENT' &&
+            'border border-dark'
           }  ${this.isSplitBooking() ? 'splitBooking' : ''}`}
           style={{ backgroundColor: legend.color }}
           onTouchStart={event => this.startDragging(event, 'move')}
