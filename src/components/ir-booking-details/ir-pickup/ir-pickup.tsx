52c2646082c7e5f92cfa8ba2bdccfc925a004278
David Chowaifaty <chowaifaty.d@gmail.com>
2024-02-05 12:27:08 +0200

    modified the styling in the sidebars

--- src/components/ir-booking-details/ir-pickup/ir-pickup.tsx
+++ src/components/ir-booking-details/ir-pickup/ir-pickup.tsx
@@ -3,16 +3,37 @@ import { Component, Host, h } from '@stencil/core';
 @Component({
   tag: 'ir-pickup',
   styleUrl: 'ir-pickup.css',
-  shadow: true,
+  scoped: true,
 })
 export class IrPickup {
-
   render() {
     return (
-      <Host>
-        <slot></slot>
+      <Host class={'card mt-1 p-1'}>
+        <section class={'d-flex align-items-center'}>
+          <h4 class={'m-0 p-0'}>Pickup</h4>
+          <ir-select
+            class={'m-0'}
+            LabelAvailable={false}
+            data={[
+              {
+                text: 'helo',
+                value: 'helklo',
+              },
+            ]}
+          ></ir-select>
+          <ir-select
+            class={'m-0'}
+            label="Arrival date"
+            data={[
+              {
+                text: 'helo',
+                value: 'helklo',
+              },
+            ]}
+          ></ir-select>
+          <ir-input-text label="Time"></ir-input-text>
+        </section>
       </Host>
     );
   }
-
 }
