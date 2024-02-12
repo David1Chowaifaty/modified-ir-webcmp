4387b508f3bfab15e54a65d38e7ad83ea281b2bc
David Chowaifaty <chowaifaty.d@gmail.com>
2024-02-12 08:43:09 +0200

    modified the dropdown icon

--- src/components/igloo-calendar/igl-to-be-assigned/igl-to-be-assigned.tsx
+++ src/components/igloo-calendar/igl-to-be-assigned/igl-to-be-assigned.tsx
@@ -282,7 +282,7 @@ export class IglToBeAssigned {
                         //onClick={() => this.showUnassignedDate()}
                       >
                         <span class="font-weight-bold">{this.data[this.selectedDate].dateStr}</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={14} width={14}>
+                        <svg class={'caret-icon'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={14} width={14}>
                           <path
                             fill="#6b6f82"
                             d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
