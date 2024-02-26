ef07cfece5262ecf279dc7c310a0c8a83870f417
david <chowaifaty.d@gmail.com>
2024-02-26 18:19:54 +0200

    added the blue highlight

--- src/components/igloo-calendar/igl-cal-body/igl-cal-body.tsx
+++ src/components/igloo-calendar/igl-cal-body/igl-cal-body.tsx
@@ -254,7 +254,11 @@ export class IglCalBody {
   getGeneralCategoryDayColumns(addClass: string, isCategory: boolean = false, index: number) {
     return calendar_dates.days.map(dayInfo => {
       return (
-        <div class={`cellData  font-weight-bold categoryPriceColumn ${addClass + '_' + dayInfo.day} ${dayInfo.day === this.today ? 'currentDay' : ''}`}>
+        <div
+          class={`cellData  font-weight-bold categoryPriceColumn ${addClass + '_' + dayInfo.day} ${
+            dayInfo.day === this.today || dayInfo.day === this.toBeAssignedDate ? 'currentDay' : ''
+          }`}
+        >
           {isCategory ? (
             <span class={'categoryName'}>
               {dayInfo.rate[index].exposed_inventory.rts}
@@ -273,7 +277,7 @@ export class IglCalBody {
     // onDragOver={event => this.handleDragOver(event)} onDrop={event => this.handleDrop(event, addClass+"_"+dayInfo.day)}
     return this.calendarData.days.map(dayInfo => (
       <div
-        class={`cellData ${'room_' + roomId + '_' + dayInfo.day} ${dayInfo.day === this.today ? 'currentDay' : ''} ${
+        class={`cellData ${'room_' + roomId + '_' + dayInfo.day} ${dayInfo.day === this.today || dayInfo.day === this.toBeAssignedDate ? 'currentDay' : ''} ${
           this.dragOverElement === roomId + '_' + dayInfo.day ? 'dragOverHighlight' : ''
         } ${this.selectedRooms.hasOwnProperty(this.getSelectedCellRefName(roomId, dayInfo)) ? 'selectedDay' : ''}`}
         onClick={() => this.clickCell(roomId, dayInfo, roomCategory)}
