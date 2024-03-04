e57c50f81de481b5706a6be7a402cb937782cb09
david <chowaifaty.d@gmail.com>
2024-03-04 13:32:50 +0200

    working on the booking view

--- src/components/igloo-calendar/igl-to-be-assigned/igl-tba-booking-view/igl-tba-booking-view.tsx
+++ src/components/igloo-calendar/igl-to-be-assigned/igl-tba-booking-view/igl-tba-booking-view.tsx
@@ -141,6 +141,7 @@ export class IglTbaBookingView {
       data: { bookingId: '----' },
     });
     this.onSelectRoom({ target: { value: '' } });
+    this.selectedRoom = -1;
     this.addToBeAssignedEvent.emit({ key: 'tobeAssignedEvents', data: [] });
     this.renderView();
   }
@@ -189,7 +190,12 @@ export class IglTbaBookingView {
             {this.highlightSection ? (
               <div class="d-flex buttonsContainer">
                 <button type="button" class="btn btn-secondary btn-sm" onClick={evt => this.handleCloseAssignment(evt)}>
-                  X
+                  <svg class="m-0 p-0" xmlns="http://www.w3.org/2000/svg" height="12" width="9" viewBox="0 0 384 512">
+                    <path
+                      fill="currentColor"
+                      d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+                    />
+                  </svg>
                 </button>
                 <ir-button
                   isLoading={isRequestPending('/Assign_Exposed_Room')}
