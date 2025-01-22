4206d9c7836e97632636e675ca0d9e54e44deaf0
david <chowaifaty.d@gmail.com>
2025-01-22 23:19:39 +0200

    Revert "modified the input mask"
    This reverts commit d3491bd9a6bc41ec37d419e8de34680bb707e526.

--- src/components/ir-input-text/ir-input-text.tsx
+++ src/components/ir-input-text/ir-input-text.tsx
@@ -220,7 +220,6 @@ export class IrInputText {
             </span>
           </label>
           <input
-            data-state={!!this.value ? '' : this.mask ? 'empty' : ''}
             ref={el => (this.inputRef = el)}
             type={this.type}
             onFocus={e => {
@@ -230,7 +229,7 @@ export class IrInputText {
             required={this.required}
             onBlur={this.handleBlur.bind(this)}
             disabled={this.disabled}
-            class={`ir-input form-control bg-white pl-0 input-sm rate-input py-0 m-0 rateInputBorder ${(this.error || this.isError) && 'danger-border'}`}
+            class={`form-control bg-white pl-0 input-sm rate-input py-0 m-0 rateInputBorder ${(this.error || this.isError) && 'danger-border'}`}
             id={this.id}
             value={this.value}
             placeholder={this.placeholder}
@@ -244,7 +243,7 @@ export class IrInputText {
       <div class={`input-group-prepend col-${this.labelWidth} p-0 text-${this.labelColor}`}>
         <label
           htmlFor={this.id}
-          class={` input-group-text ${this.labelPosition === 'right' ? 'justify-content-end' : this.labelPosition === 'center' ? 'justify-content-center' : ''} ${
+          class={`input-group-text ${this.labelPosition === 'right' ? 'justify-content-end' : this.labelPosition === 'center' ? 'justify-content-center' : ''} ${
             this.labelBackground ? 'bg-' + this.labelBackground : ''
           } flex-grow-1 text-${this.labelColor} border-${this.labelBorder === 'none' ? 0 : this.labelBorder} `}
         >
@@ -262,17 +261,17 @@ export class IrInputText {
     if (this.required && !this.initial) {
       className = `${className} border-danger`;
     }
+
     return (
       <div class="form-group">
         <div class="input-group row m-0">
           {label}
           <input
-            data-state={!!this.value ? '' : this.mask ? 'empty' : ''}
             id={this.id}
             ref={el => (this.inputRef = el)}
             readOnly={this.readonly}
             type={this.type}
-            class={`ir-input ${className} ${this.error || this.isError ? 'border-danger' : ''} form-control-${this.size} text-${this.textSize} col-${
+            class={`${className} ${this.error || this.isError ? 'border-danger' : ''} form-control-${this.size} text-${this.textSize} col-${
               this.LabelAvailable ? 12 - this.labelWidth : 12
             } ${this.readonly && 'bg-white'} ${this.inputStyles}`}
             onBlur={this.handleBlur.bind(this)}
