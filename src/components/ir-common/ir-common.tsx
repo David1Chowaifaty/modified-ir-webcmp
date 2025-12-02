c1b36367112f74e1c920af5bae88355de6dc29f4
david <chowaifaty.d@gmail.com>
2025-12-02 12:29:47 +0200

    Refactor IrBookingListing and IrCommon components for improved modularity and styling
    - Removed height property from IrBookingListing CSS to enhance layout flexibility.
    - Simplified imports in IrCommon component by removing unnecessary Host import, streamlining the codebase.

--- src/components/ir-common/ir-common.tsx
+++ src/components/ir-common/ir-common.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, Prop, State, Watch, h } from '@stencil/core';
+import { Component, Prop, State, Watch } from '@stencil/core';
 import { IrOnlineResource } from '../../common/models';
 import { onlineResources } from '../../common/ir.common.resources';
 
